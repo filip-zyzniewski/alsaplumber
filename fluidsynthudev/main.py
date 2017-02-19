5e531936981040eb1be518d24c4da3dde7a07d55
Filip Zyzniewski <filip.zyzniewski@gmail.com>
2017-02-19 14:45:13 +0000

    main: some trivial initial code.

--- fluidsynthudev/main.py
+++ fluidsynthudev/main.py
@@ -1,4 +1,13 @@
 'Provides the entry point.'
 
+import pyalsa.alsaseq
+
 def main():
-    pass
+    seq = pyalsa.alsaseq.Sequencer(
+        name = 'default',
+        clientname = 'fluidsynthudev',
+        streams = pyalsa.alsaseq.SEQ_OPEN_DUPLEX,
+        mode = pyalsa.alsaseq.SEQ_BLOCK,
+    )
+    for name, id, connected in seq.connection_list():
+        print name, id, connected
